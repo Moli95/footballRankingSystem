00f8e8c52b663f1f0b4d0f66a54c43ff2b3b995e
Tomasz Swirski <tomaszswirski@wp.pl>
2017-10-25 21:49:39 +0200

    Added connection with a database and add new view for ranking system

--- controllers/addPlayer.js
+++ controllers/addPlayer.js
@@ -1,37 +1,54 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [
-	{
-		playerName: "Tom",
-		points: 1200
-	},
-	{
-		playerName: "Mark"
-		points: 1300
-	},
-	{
-		playerName: "Kati",
-		points: 800
-	}
-];
+// Connect to the database
+
+mongoose.connect('mongodb://admin:admin@ds233895.mlab.com:33895/frs');
+
+// create a schema - this is like a blueprint
+
+var frsSchema = new mongoose.Schema({
+	playerName: String,
+	points: Number
+});
+
+var Frs = mongoose.model('players', frsSchema);
+
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
 
 app.get('/addPlayer', function(req, res) {
-	res.render('addPlayer', {players: data});
+	// get data form mongodb and pass it to the view
+	// Todo.find({item: 'Tom'}) // if I want one item
+	Frs.find({}, function(err, data) {
+		if (err) throw err;
+		res.render('addPlayer', {players: data});
+	});
 });
 
 app.post('/addPlayer', urlencodedParser, function(req, res) {
-	data.push(req.body);
-	res.json(data);
-	console.log(data);
+	// get data from the view and add it to mongoDB
 
+	var newPlayersList = Frs(req.body).save(function(err, data) {
+		if (err) throw err;
+		res.json(data);
+	});
 });
 
 app.delete('/addPlayer', function(req, res) {
-	
+	// delete the requested item from mongoDB
+
+});
+
+
+
+app.get('/ranking', function(req, res) {
+	Frs.find({}, function(err, data) {
+		if (err) throw err;
+		res.render('ranking', {players: data});
+	});
 });
 
 }
